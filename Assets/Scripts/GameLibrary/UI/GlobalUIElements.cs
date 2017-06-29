a398c32d019d9151a6dcfd21e55f20ee06c2704b
danielpmcconkey <danielpmcconkey@gmail.com>
2017-06-29 12:07:54 -0400

    Add files via upload

---
+++ Assets/Scripts/GameLibrary/UI/GlobalUIElements.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GlobalUIElements
+{
+    public static Text debugText;
+    public static Material miniMapLineMaterial;
+    public static GameObject minimapPanel;
+}
